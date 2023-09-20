bba61ee5bfd9b47dff6df02f308e1506680de234
Limuncic <145533801+Limuncic@users.noreply.github.com>
2023-09-20 08:45:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace varijableIspisPodataka
+{
+    
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int a, b, x, y; // deklaracija
+            a = 100; // definicija
+            b = 3;
+
+            x = a / b;
+            y = a % b;
+
+            Console.WriteLine(a + " dijeljeno sa " + b +" je " + x + " s ostatkom " + y);
+
+            Console.ReadKey();
+        }
+    }
+}
